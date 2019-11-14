45a2368658072232d7e6c38f45513803882330ba
DRMD431LU <drmdfoureilu@gmail.com>
2019-11-14 15:26:16 -0600

    status_code 302 and redirect

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_protect
-
+from .models import Item
 # Create your views here.
 #@csrf_protect
 def home_page(request):
 	if request.method == 'POST':
-		return render(request, 'home.html',{
-			'new_item_text': request.POST.get('item_text','Buy stuff'),
+		new_item_text = request.POST['item_text']
+		Item.objects.create(text=new_item_text)
+	else:
+		new_item_text = ''
+
+	return render(request, 'home.html',{
+			'new_item_text': new_item_text,
 			})
-	#	return HttpResponse(request.POST['item_text'])
-	return render(request, 'home.html')
-	#return HttpResponse('<html><title>To-Do lists</title></html>')
