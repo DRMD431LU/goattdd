a3b72d1d731e6fe970a542e43d1b1dbf6ac14e5e
DRMD431LU <drmdfoureilu@gmail.com>
2019-11-15 16:58:00 -0600

    redirect urls as restful design

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -7,10 +7,10 @@ from .models import Item
 def home_page(request):
 	if request.method == 'POST':
 		#print("hola")
-		print(request.POST['text'])
+		#print(request.POST['text'])
 		Item.objects.create(text=request.POST.get('text', 'stuff'))
 		#Item.objects.create(text=request.POST.get('item_text', 'Use it to fly'))
-		return redirect('/')
+		return redirect('/lists/the-only-list-in-the-world/')
 
 	items = Item.objects.all()
 	return render(request,'home.html', {'items': items})
